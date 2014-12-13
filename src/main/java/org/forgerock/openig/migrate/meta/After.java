4730f9d297e620e1911a0a74b2a5d4e124437352
Guillaume Sauthier <guillaume.sauthier@forgerock.com>
2014-12-13 22:20:19 +0100

    Introduces annotation to declares relationships between Action.
    This is not actually backed by any code-enforcement

---
+++ src/main/java/org/forgerock/openig/migrate/meta/After.java
@@ -0,0 +1,33 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2014 ForgeRock AS.
+ */
+
+package org.forgerock.openig.migrate.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.forgerock.openig.migrate.Action;
+
+/**
+ * Declares that the annotated action has to be semantically executed <b>after</b> another one.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface After {
+    Class<? extends Action>[] value();
+}
